800b975b58b18481dc32c103b211e5e0d1782d92
Sahith Kumar <sahithkumar1999@gmail.com>
2023-04-27 01:20:59 +0200

    creating files and naming conventions for coding

---
+++ Shape-matching-using-a-dynamic-time-warping-algorithm/dynamic_time_warping.py
@@ -0,0 +1,6 @@
+# shape_matching.py
+
+# function for shape matching
+def shape_matching_algorithm(shape1, shape2):
+    # your code for shape matching here
+    pass
---
+++ Shape-matching-using-a-dynamic-time-warping-algorithm/test_dynamic_time_warping.py
@@ -0,0 +1,6 @@
+# test_shape_matching.py
+
+import unittest
+from dynamic_time_warping import dynamic_time_warping_algorithm
+
+#class TestShapeMatching(unittest.TestCase):
