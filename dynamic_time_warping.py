3e99557cf1d89a076dd98b44dfe19838b6587c19
Sahith Kumar <sahithkumar1999@gmail.com>
2023-10-09 00:57:13 +0200

    Image displayed in GUI

--- dynamic_time_warping.py
+++ dynamic_time_warping.py
@@ -8,43 +8,51 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import PhotoImage
+from tkinter import filedialog
+from PIL import Image, ImageTk
+import shutil
+
 
 # Create the main application window
 app = tk.Tk()
 app.title("Shape Matching Application")
 app.geometry("800x600")
 
-
-
 # Create a Text widget for displaying text
-text_widget = tk.Text(app, wrap=tk.WORD)  # Use wrap=tk.WORD to wrap text at word boundaries
-text_widget.pack(fill=tk.BOTH, expand=True)  # Fill and expand to take up the entire window
+text_widget = tk.Text(app, wrap=tk.WORD)
+text_widget.pack(fill=tk.BOTH, expand=True)
 
 # Insert text into the Text widget
 text_widget.insert(tk.END, "Shape Matching with Dynamic Time Warping\n", ("main_heading",))
 text_widget.insert(tk.END, "Subheading\n\n", ("sub_heading",))
 text_widget.insert(tk.END, "In the world of computer vision and pattern recognition, the task of shape matching plays a critical role. Shape matching involves finding similarities or correspondences between two shapes, which can have numerous applications, including image recognition, object tracking, and gesture recognition. One powerful technique for shape matching is the Dynamic Time Warping (DTW) algorithm.")
 
-
-
-
 # Configure text tags for formatting
 text_widget.tag_configure("main_heading", font=("Helvetica", 20, "bold"))
 text_widget.tag_configure("sub_heading", font=("Helvetica", 16, "underline"))
 
-app.mainloop()
+# Create a label for displaying the matched image with a placeholder text
+image_label = tk.Label(app, text="Uploaded Image Will Be Displayed Here")
+image_label.pack()
 
-# Load the pre-trained face detection model
-face_cascade = cv2.CascadeClassifier('D:\sem 2\Computational Intelligence\Project\Shape-matching-using-a-dynamic-time-warping-algorithm\haarcascade_frontalface_default.xml')
-photo_name = "Sundar Pichai"
+# Create an "Upload Image" button
+upload_button = tk.Button(app, text="Upload Image")
+upload_button.pack()
 
 # Create a folder to store the output images
-output_folder = 'output_images ' + photo_name
+output_folder = 'output_images'
 os.makedirs(output_folder, exist_ok=True)
+
+# Create a folder to store the output graphs
+output_graph_folder = 'output_Graph'
+os.makedirs(output_graph_folder, exist_ok=True)
+
+# Load the pre-trained face detection model
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+photo_name = "Sundar Pichai"
 
 # Create an empty list to store the data frames
 data_frames = []
-
 
 # Function to perform DTW shape matching and visualize the result
 def shape_matching_with_visualization(template_sequence, target_sequence, threshold):
@@ -52,16 +60,14 @@ def shape_matching_with_visualization(template_sequence, target_sequence, thresh
     distance, path = fastdtw(template_sequence, target_sequence)
 
     if distance < threshold:
-        return True, distance, path   # Return True if the match is below the threshold
+        return True, distance, path
     else:
-        return False, distance, path  # Return False if the match is above the threshold
-
+        return False, distance, path
 
 # Function to save the binary data to an Excel file
 def save_data_to_excel(data_frames, output_file):
     data = pd.concat(data_frames, ignore_index=True)
-    data.to_excel(output_file, index=False)    # Save the data frames as an Excel file
-
+    data.to_excel(output_file, index=False)
 
 # Function to perform DTW shape matching
 def shape_matching(template_sequence, target_sequence, threshold):
@@ -74,79 +80,140 @@ def shape_matching(template_sequence, target_sequence, threshold):
         return False, distance
 
 # Load and preprocess the template sequences for shape matching
-template_sequences = {}  # Store template sequences here
+template_sequences = {}
 
 # Iterate over the images in the 'images' folder
 for filename in os.listdir('images'):
     if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
         # Load the image
         image_path = os.path.join('images', filename)
-        template_image = cv2.imread(image_path)    # Load a template image
-        template_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+        template_image = cv2.imread(image_path)
+        template_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
 
         # Extract the contour of the template image
         contour, _ = cv2.findContours(template_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        template_sequence = np.squeeze(contour)  # Flatten to a 2D array
-        template_sequences[filename] = template_sequence # Store the template sequence
-
-# Load the given image for shape matching
-given_image_path = 'Check/Check2.jpeg'  # Replace with the path to your given image
-given_image = cv2.imread(given_image_path)  # Load the given image
-given_gray = cv2.cvtColor(given_image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-
-matching_results = []
-
-# Extract contour from the given grayscale image
-given_contour, _ = cv2.findContours(given_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-given_sequence = np.squeeze(given_contour)
-
-# Perform shape matching with template sequences
-for template_filename, template_sequence in template_sequences.items():
-    match, distance, path = shape_matching_with_visualization(template_sequence, given_sequence, threshold=200)
-
-    if match:
-        print(f"Given image matched with {template_filename} (DTW Distance: {distance})")
-
-        # Find peak positions
-        max_template_position = np.argmax(template_sequence, axis=0)  # Find peak position in the template sequence
-        max_given_position = np.argmax(given_sequence, axis=0)   # Find peak position in the given sequence
-        
-        print("Peak position in the template sequence:", max_template_position)
-        print("Peak position in the given sequence:", max_given_position)
-
-        # Extract the x and y coordinates of the matched points
-        matched_x = [template_sequence[i, 0] for i, _ in path]
-        matched_y = [template_sequence[i, 1] for i, _ in path]
-
-        # Plot the sequences and highlight the matched points
-        plt.figure(figsize=(8, 4))
-        plt.plot(template_sequence[:, 0], template_sequence[:, 1], label='Template Sequence', marker='o', markersize=5)
-        plt.plot(given_sequence[:, 0], given_sequence[:, 1], label='Given Sequence', marker='x', markersize=5)
-        plt.scatter(matched_x, matched_y, c='r', label='Matched Points')  # Plot matched points in red
-        plt.legend()
-        plt.title(f"Matching Result with {template_filename} (DTW Distance: {distance})")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.show()
-
-    matching_results.append((template_filename, match, distance))  # Store the matching results
+        template_sequence = np.squeeze(contour)
+        template_sequences[filename] = template_sequence
 
 
-# Sort matching results by distance
-matching_results.sort(key=lambda x: x[2])
+# Function to handle shape matching
+def match_shape(input_image_path):
+    matching_results_text.delete(1.0, tk.END)  # Clear previous matching results
 
-# Display and save the matched images along with matching values
-for result in matching_results:
-    template_filename, match, distance = result
-    if match:
-        print(f"Given image matched with {template_filename} (DTW Distance: {distance})")
-        # Save the matched image with a filename indicating the match
-        matched_image_filename = f'{given_image_path}_matched_with_{template_filename}.jpg'
-        cv2.imwrite(os.path.join(output_folder, matched_image_filename), given_image)
+    # Load the given image for shape matching
+    given_image = cv2.imread(input_image_path)
+    given_gray = cv2.cvtColor(given_image, cv2.COLOR_BGR2GRAY)
 
-# Save the matching results to an Excel file
-output_file = 'matching_results ' + photo_name + '.xlsx'
-matching_data = pd.DataFrame({'Template': [result[0] for result in matching_results], 'DTW Distance': [result[2] for result in matching_results]})
-save_data_to_excel([matching_data], output_file)
+    matching_results = []
 
-print("Matching results saved to Excel.")
+    # Extract contour from the given grayscale image
+    given_contour, _ = cv2.findContours(given_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    given_sequence = np.squeeze(given_contour)
+
+    # Perform shape matching with template sequences
+    for template_filename, template_sequence in template_sequences.items():
+        match, distance, path = shape_matching_with_visualization(template_sequence, given_sequence, threshold=200)
+
+        if match:
+            print(f"Given image matched with {template_filename} (DTW Distance: {distance})")
+
+            # Find peak positions
+            max_template_position = np.argmax(template_sequence, axis=0)
+            max_given_position = np.argmax(given_sequence, axis=0)
+
+            print("Peak position in the template sequence:", max_template_position)
+            print("Peak position in the given sequence:", max_given_position)
+
+            # Extract the x and y coordinates of the matched points
+            matched_x = [template_sequence[i, 0] for i, _ in path]
+            matched_y = [template_sequence[i, 1] for i, _ in path]
+
+            # Plot the sequences and highlight the matched points
+            plt.figure(figsize=(8, 4))
+            plt.plot(template_sequence[:, 0], template_sequence[:, 1], label='Template Sequence', marker='o', markersize=5)
+            plt.plot(given_sequence[:, 0], given_sequence[:, 1], label='Given Sequence', marker='x', markersize=5)
+            plt.scatter(matched_x, matched_y, c='r', label='Matched Points')
+            plt.legend()
+            plt.title(f"Matching Result with {template_filename} (DTW Distance: {distance})")
+            plt.xlabel("X")
+            plt.ylabel("Y")
+
+            # Save the graph image in the "output_Graph" folder
+            graph_image_path = os.path.join(output_graph_folder, f'graph.png')
+            plt.savefig(graph_image_path, bbox_inches='tight')  # Save the graph image
+            plt.close()  # Close the plot to release resources
+
+            # Display the graph image in the GUI
+            graph = Image.open(graph_image_path)
+            graph.thumbnail((400, 400))
+            graph_photo = ImageTk.PhotoImage(graph)
+            graph_label.config(image=graph_photo)
+            graph_label.image = graph_photo  # Keep a reference to prevent it from being garbage collected
+
+        matching_results.append((template_filename, match, distance))
+
+    # Sort matching results by distance
+    matching_results.sort(key=lambda x: x[2])
+
+    # Save the matching results to an Excel file
+    output_file = 'matching_results ' + photo_name + '.xlsx'
+    matching_data = pd.DataFrame({'Template': [result[0] for result in matching_results], 'DTW Distance': [result[2] for result in matching_results]})
+    save_data_to_excel([matching_data], output_file)
+
+    # Display matching results in the GUI
+    matching_results_text.insert(tk.END, "Matching Results:\n")
+    for result in matching_results:
+        if result[1]:
+            matching_results_text.insert(tk.END, f"Given image matched with {result[0]} (DTW Distance: {result[2]})\n")
+
+            # Load and display the matched image
+            matched_image_path = os.path.join(output_graph_folder, f'graph.png')
+            matched_image = Image.open(matched_image_path)
+            matched_image.thumbnail((400, 400))  # Resize the image if needed
+            matched_image = ImageTk.PhotoImage(matched_image)
+            image_label.config(image=matched_image, text="")  # Update the image label with the matched image
+            image_label.image = matched_image  # Keep a reference to prevent image from being garbage collected
+
+        #else:
+            #matching_results_text.insert(tk.END, f"No match found with {result[0]} (DTW Distance: {result[2]})\n")
+    matching_results_text.insert(tk.END, "\nResults saved to Excel.")
+
+# Function to handle image upload
+def upload_image():
+    # Open a file dialog to select an image file
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png")])
+
+    if file_path:
+        # Define the destination path and file name
+        destination_path = 'output_Graph'
+        destination_file = 'graph.jpg'
+
+        # Copy the selected image to the destination
+        shutil.copy(file_path, os.path.join(destination_path, destination_file))
+
+        # Display a confirmation message in the text widget
+        confirmation_text = f"Image '{destination_file}' uploaded successfully to '{destination_path}'."
+        text_widget.insert(tk.END, "\n\n" + confirmation_text)
+        text_widget.see(tk.END)
+
+        # Perform shape matching with the uploaded image
+        match_shape(os.path.join(destination_path, destination_file))
+
+
+
+
+
+
+# Create an "Upload Image" button
+upload_button.config(command=upload_image)
+
+# Create a label for displaying the matching results
+matching_results_text = tk.Text(app, wrap=tk.WORD)
+matching_results_text.pack(fill=tk.BOTH, expand=True)
+
+# Create a label for displaying the graph image
+graph_label = tk.Label(app, text="Matching Graph")
+graph_label.pack()
+
+# Start the main tkinter event loop
+app.mainloop()
